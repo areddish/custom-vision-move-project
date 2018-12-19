508884f2c78656531be2a47a01856fdb7be84aa7
Andrew Reddish <areddish@microsoft.com>
2018-12-19 13:38:04 -0800

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# custom-vision-move-project
+# Utility for moving projects between subscriptions for Microsoft Cognitive Services - Custom Vision service.
 Sample script to move a Custom Vision project from one Subscription/Region to another.
+
+# Running this script
+
+To run you need three pieces of information:
+From the settings page of the source Subscription (_where you wnat to move *from*_)
+*Source Training Key
+*Source Project Id
+From the settings page of the destination Subscription (_where you want to move *to*_)
+*Destination Training Key
+
+To run the script you first install the requirements
+```
+pip install requirements
+```
+
+Then run the python script with the necessary information:
+```Python
+python migrate_project.py -p <project id> -s <source training key> -d <destination training key>
+```
+
+This script will recreate a project with the destination training-key and download/upload all of the tags, regions, and images. It will leave you with a new project in your new subscription with no trained iterations, from here you can train a new iteration.
+
+
---
+++ migrate_project.py
@@ -0,0 +1,91 @@
+# Migrate a project
+
+import os
+import sys
+import time
+
+import argparse
+
+from azure.cognitiveservices.vision.customvision.training import training_api
+from azure.cognitiveservices.vision.customvision.training.models import ImageUrlCreateBatch, ImageUrlCreateEntry, Region
+
+def migrate_tags(src_trainer, dest_trainer, project_id, dest_project_id):
+    tags =  src_trainer.get_tags(project_id)
+    print ("Found:", len(tags), "tags")
+    # Re-create all of the tags and store them for look-up
+    created_tags = {}
+    for tag in src_trainer.get_tags(project_id):
+        print ("Creating tag:", tag.name, tag.id)
+        created_tags[tag.id] = dest_trainer.create_tag(dest_project_id, tag.name, description=tag.description).id
+    return created_tags
+
+def migrate_images(src_trainer, dest_trainer, project_id, dest_project_id, created_tags):
+    # Migrate any tagged images that may exist and preserve their tags and regions.
+    count = src_trainer.get_tagged_image_count(project_id)
+    print ("Found:",count,"tagged images.")
+    migrated = 0
+    while(count > 0):
+        count_to_migrate = min(count, 50)
+        print ("Getting", count_to_migrate, "images")
+        images = src_trainer.get_tagged_images(project_id, take=count_to_migrate, skip=migrated)
+        for i in images:
+            print ("Migrating", i.id, i.image_uri)
+            regions = []
+            for r in i.regions:
+                print ("Found region:", r.region_id, r.tag_id, r.left, r.top, r.width, r.height)
+                regions.append(Region(tag_id=created_tags[r.tag_id], left=r.left, top=r.top, width=r.width, height=r.height))
+            
+            entry = ImageUrlCreateEntry(url=i.image_uri, regions=regions)
+            dest_trainer.create_images_from_urls(dest_project_id, images=[entry])
+        migrated += count_to_migrate
+        count -= count_to_migrate
+
+    # Migrate any untagged images that may exist.
+    count = src_trainer.get_untagged_image_count(project_id)
+    print ("Found:", count, "untagged images.")
+    migrated = 0
+    while(count > 0):
+        count_to_migrate = min(count, 50)
+        print ("Getting", count_to_migrate, "images")
+        images = src_trainer.get_untagged_images(project_id, take=count_to_migrate, skip=migrated)
+        for i in images:
+            print ("Migrating", i.id, i.image_uri)
+            regions = []
+            for r in i.regions:
+                print ("Found region:", r.region_id, r.tag_id, r.left, r.top, r.width, r.height)
+                regions.append(Region(tag_id=created_tags[r.tag_id], left=r.left, top=r.top, width=r.width, height=r.height))
+            
+            entry = ImageUrlCreateEntry(url=i.image_uri, regions=regions)
+            dest_trainer.create_images_from_urls(dest_project_id, images=[entry])
+        migrated += count_to_migrate
+        count -= count_to_migrate
+    return images
+
+def migrate_project(src_trainer, dest_trainer, project_id):
+    # Get the original project
+    src_project = src_trainer.get_project(project_id)
+    # Create the destination project
+    return dest_trainer.create_project(src_project.name, description=src_project.description, domain_id=src_project.settings.domain_id)
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-p", "--project", action="store", type=str, help="Source project ID", dest="project_id", default=None)
+    arg_parser.add_argument("-s", "--src", action="store", type=str, help="Source Training-Key", dest="source_training_key", default=None)
+    arg_parser.add_argument("-d", "--dest", action="store", type=str, help="Destination Training-Key", dest="destination_training_key", default=None)
+    args = arg_parser.parse_args()
+
+    if (not args.project_id or not args.source_training_key or not args.destination_training_key):
+        arg_parser.print_help()
+        exit(-1)
+
+    print ("Collecting information for source project:", args.project_id)
+
+    # Client for Source
+    src_trainer = training_api.TrainingApi(args.source_training_key)
+
+    # Client for Destination
+    dest_trainer = training_api.TrainingApi(args.destination_training_key)
+
+    destination_project = migrate_project(src_trainer, dest_trainer, args.project_id)
+    tags = migrate_tags(src_trainer, dest_trainer, args.project_id, destination_project.id)
+    source_images = migrate_images(src_trainer, dest_trainer, args.project_id, destination_project.id, tags)
